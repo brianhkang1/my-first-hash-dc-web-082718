373798b056e97f591b65dde52955dc37f43a6016
Brian Kang <brianhkang1@gmail.com>
2018-08-14 20:35:44 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,8 +17,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -28,7 +27,7 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+  shipping_manifest["mustkets" => 2]
 
 
   # add 4 gun powder to the shipping_manifest hash below
